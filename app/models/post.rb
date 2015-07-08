23e5b82ae82cdae2998f4bc8d36d73c66131b049
Alloffices <alloffices.nyc@gmail.com>
2015-07-08 05:33:37 -0400

    added some more comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
+
+	#validations ensure that valid data is saved into our database
+	#for example: every user email that is submitted need to be validated
+
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
-	
+
 end
